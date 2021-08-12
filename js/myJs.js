3efbc468258c62f414de2b6aa262e22aaabf4abf
TuanLamAnh <tuanlam.31191026900@st.ueh.edu.vn>
2021-08-12 19:40:55 +0700

    change text

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hí, con quỷ :33",
+  text2: "T có điều này muốn hỏi, m nhớ phải trả lời thật lòng nhaaa.",
+  text3: "m đang nhớ t vl đúng không con quỷ :v",
+  text4: "Nếu m ko trả lời mà thoát ra tức là muốn làm vợ t rùi đó nha :v",
+  text5: "Không hề",
+  text6: "Nhớ ơi là nhớ ý <3",
+  text7: "lí do m nhớ t đi :vvvv",
+  text8: "Gửi cho t <3",
+  text9: "Vì bạn Tún người iu tui đẹp trai vờ lờ <3",
+  text10: "t biết mà ^^ Yêu m 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Lát nữa rảnh call video hong ??? Còn giờ thì chờ gì nữa mà ko inbox cho t đi nàooo",
   text12: "Okii lunn <3",
 };
 
